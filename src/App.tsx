523a6e699053b6a5743d0a95ec8d753cf232bcb2
Siddhartha Bhagwan <siddharthabhagwan@gmail.com>
2019-08-07 15:28:06 -0400

    Testinf framework in place - Enzyme + Jest. Minor code cleanup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import MapContainer from "./components/MapContainer";
-import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <MapContainer />
       </div>
     );
---
+++ src/utils/confix.test.tsx
@@ -0,0 +1,32 @@
+import { getDefaultDayDetails, getDefaultMapState } from "./config";
+
+test("getDefaultmapState", () => {
+  const defaultMapState = getDefaultMapState();
+  expect(defaultMapState).toMatchObject({
+    url: "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+    attribution:
+      '<a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    center: [27.840457443855108, 86.76420972837559],
+    zoom: 11.4,
+    zoomSnap: 0.1,
+    hoverColor: "#1EBBD7",
+    markerZoom: 16,
+    style: { height: "100vh", width: "100%" },
+    zoomDuration: 0.5,
+    topLeftPadding: [0, 50],
+    bottomRightPadding: [0, 150]
+  });
+});
+
+test("getDefaultDayDetails", () => {
+  const defaultDayDetails = getDefaultDayDetails();
+  expect(defaultDayDetails).toMatchObject({
+    day: "0",
+    name: "Fly from Kathmandu to Lukla",
+    time: "0h 00m",
+    distance: "0 mi / 0 km",
+    start_alt: "0",
+    end_alt: "0",
+    peak_alt: ""
+  });
+});
