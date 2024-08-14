2507e537ef8eb2d3cc8e147b5884f3d5dcee6e06
Siddharth Bhagwan <siddharthabhagwan@siddharths-air.home>
2024-08-14 17:10:01 +0200

    GA Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from "react";
+import ReactGA from "react-ga4";
 import MapContainer from "./components/MapContainer";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <MapContainer />
-      </div>
-    );
-  }
-}
+ReactGA.initialize("G-0D6NC98JBF", {
+  gaOptions: {
+    debug_mode: true,
+  },
+  gtagOptions: {
+    debug_mode: true,
+  },
+});
+
+const App = () => {
+  return (
+    <div>
+      <MapContainer />
+    </div>
+  );
+};
 
 export default App;
