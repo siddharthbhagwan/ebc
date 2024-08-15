2c7a118fc1e0b1b635f514b057ae8775df1070cf
Siddhartha Bhagwan <siddharthabhagwan@gmail.com>
2024-08-15 10:37:49 +0200

    Favicon added. Panels repositioned

--- src/components/Reset.js
+++ src/components/Reset.js
@@ -1,50 +1,50 @@
-import React from 'react';
-import { withLeaflet } from 'react-leaflet';
-import { connect } from 'react-redux';
-import resetIcon from '../resources/images/map.svg';
-import legendIcon from '../resources/images/legend.svg';
-import 'leaflet-easybutton';
-import Control from 'react-leaflet-control';
+import React from "react";
+import { withLeaflet } from "react-leaflet";
+import { connect } from "react-redux";
+import resetIcon from "../resources/images/map.svg";
+import legendIcon from "../resources/images/legend.svg";
+import "leaflet-easybutton";
+import Control from "react-leaflet-control";
 
 const Reset = (props) => {
-	const { center, zoom, setLegend } = props;
-	const { map } = props.leaflet;
+  const { center, zoom, setLegend } = props;
+  const { map } = props.leaflet;
 
-	const resetZoom = () => map.flyTo(center, zoom, { duration: 0.5 });
+  const resetZoom = () => map.flyTo(center, zoom, { duration: 0.5 });
 
-	const toggleLegend = () => setLegend((legend) => !legend);
+  const toggleLegend = () => setLegend((legend) => !legend);
 
-	const mapButton = (
-		<img
-			width={'30px'}
-			src={resetIcon}
-			className={'icon'}
-			onClick={resetZoom}
-		/>
-	);
+  const mapButton = (
+    <img
+      width={"34px"}
+      src={resetIcon}
+      className={"icon"}
+      onClick={resetZoom}
+    />
+  );
 
-	const legendButton = (
-		<img
-			width={'30px'}
-			src={legendIcon}
-			className={'icon'}
-			onClick={toggleLegend}
-		/>
-	);
+  const legendButton = (
+    <img
+      width={"34px"}
+      src={legendIcon}
+      className={"icon"}
+      onClick={toggleLegend}
+    />
+  );
 
-	return (
-		<Control position="topleft">
-			<div style={{ marginBottom: 3, backgroundColor: 'white' }}>
-				{mapButton}
-			</div>
-			<div style={{ backgroundColor: 'white' }}>{legendButton}</div>
-		</Control>
-	);
+  return (
+    <Control position="topleft">
+      <div style={{ marginBottom: 3, backgroundColor: "white" }}>
+        {mapButton}
+      </div>
+      <div style={{ backgroundColor: "white" }}>{legendButton}</div>
+    </Control>
+  );
 };
 
 const mapStateToProps = (state) => ({
-	zoom: state.mapState.zoom,
-	center: state.mapState.center,
+  zoom: state.mapState.zoom,
+  center: state.mapState.center,
 });
 
 export default connect(mapStateToProps)(withLeaflet(Reset));
