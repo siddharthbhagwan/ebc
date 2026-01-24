a4832be982ea8f12f97c4772affba333be0b452c
Siddhartha Bhagwan <siddharthabhagwan@gmail.com>
2026-01-24 14:24:59 +0100

    Dashboard Updated

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,50 +2,78 @@ import React from "react";
 import { withLeaflet } from "react-leaflet";
 import { connect } from "react-redux";
 import Control from "react-leaflet-control";
+import { isMobile } from "react-device-detect";
 import "../resources/css/dashboard.css";
 
 const Dashboard = (props) => {
-  const { peakAlt, startAlt, endAlt, distance, time, icon } = props;
+  const {
+    peakAlt = null,
+    startAlt = null,
+    endAlt = null,
+    distance,
+    time,
+    icon,
+  } = props;
 
-  const getStartAlt = (startAlt) => (startAlt ? `${startAlt} ft` : "");
-  const getPeakAlt = (peakAlt) => (peakAlt ? ` - ${peakAlt} ft` : "");
-  const getEndAlt = (endAlt) => (endAlt ? ` - ${endAlt} ft` : "");
-  const getTimeDist = (props) => {
-    if (distance && time)
-      return (
-        <div>
-          {distance}
-          <br />
-          {time}
-        </div>
-      );
-
-    return "";
-  };
+  //  no altitude data
+  const isPlace = startAlt === "0" && endAlt === "0";
+  const position = isMobile ? "topright" : "bottomright";
 
   return (
-    <Control position={"bottomright"}>
+    <Control position={position}>
       <div className={"dashboard"}>
-        <h5>EBC 3 Pass Trek, Nepal</h5>
-        <br />
-        <div className={"dashboardDetails"}>
-          <div>
-            <span>
-              <img src={icon} width={"25px"} />{" "}
-            </span>
-            <span>Day {props.day}</span>
+        <span style={{ fontSize: 13, textAlign: "left" }}>
+          EBC 3 Pass Trek, Nepal{" "}
+        </span>
+
+        {/* Day */}
+        <span className="desc" style={{ fontWeight: "bold" }}>
+          Day {props.day}
+        </span>
+
+        {!isMobile ? <br /> : null}
+        <div
+          className={"dashboardDetails container"}
+          style={{
+            fontSize: isMobile ? 15 : 17,
+            justifyContent: isPlace ? "center" : "space-evenly",
+            maxWidth: isMobile ? 300 : "auto",
+            alignItems: "center",
+          }}
+        >
+          {!isPlace && distance && time ? (
+            <div
+              className={!isPlace ? "" : "column-30"}
+              style={{
+                flexDirection: isMobile ? "row" : "column",
+                display: "flex",
+              }}
+            >
+              {/* Time */}
+              <div className="item">{distance.split("/")[1]}</div>
+              <div className="item">{time}</div>
+            </div>
+          ) : null}
+
+          <div
+            className="column"
+            style={{
+              justifyContent: isPlace ? "center" : "space-evenly",
+            }}
+          >
+            {/* Place */}
+            <div style={{ textAlign: "center" }} className="item">
+              {props.name}
+            </div>
+            {/* Alt Details */}
+            {!isPlace ? (
+              <div className="">
+                {startAlt ? `${startAlt} ft` : ""}
+                {peakAlt ? ` - ${peakAlt} ft` : ""}
+                {endAlt ? ` - ${endAlt} ft` : ""}
+              </div>
+            ) : null}
           </div>
-          <br />
-          <div>
-            <span>{props.name}</span>
-            <br />
-            {getStartAlt(startAlt)}
-            {getPeakAlt(peakAlt)}
-            {getEndAlt(endAlt)}
-            <br />
-          </div>
-          <br />
-          {getTimeDist(props)}
         </div>
       </div>
     </Control>
