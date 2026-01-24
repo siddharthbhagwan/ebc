553163cc05028c8adec5b348fdd68be7fa447ae5
Siddhartha Bhagwan <siddharthabhagwan@gmail.com>
2026-01-24 18:43:56 +0100

    Updates

---
+++ precache-manifest.005978261b3761fd4b5a172599d96c7a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba9ea3b5e1d1d6edbd0096c51512db8",
+    "url": "/ebcd/index.html"
+  },
+  {
+    "revision": "723ab385731ac1695003",
+    "url": "/ebcd/static/css/main.652f6ec8.chunk.css"
+  },
+  {
+    "revision": "67d91a0d0580d2eb52f5",
+    "url": "/ebcd/static/js/2.e2a2fbe5.chunk.js"
+  },
+  {
+    "revision": "e814e956e87b64060b0351e69977a48e",
+    "url": "/ebcd/static/js/2.e2a2fbe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "723ab385731ac1695003",
+    "url": "/ebcd/static/js/main.596a65c8.chunk.js"
+  },
+  {
+    "revision": "71dc34b946d873911138",
+    "url": "/ebcd/static/js/runtime-main.c53bcb28.js"
+  },
+  {
+    "revision": "a9339a6512ec933e9f9a5730dd9dc5f4",
+    "url": "/ebcd/static/media/airport.a9339a65.svg"
+  },
+  {
+    "revision": "3605fab2f6f4675ee7cab8c83d4ba7b1",
+    "url": "/ebcd/static/media/ebc.3605fab2.svg"
+  },
+  {
+    "revision": "90e020248470db3f87b6974aa197231f",
+    "url": "/ebcd/static/media/leftArrow.90e02024.svg"
+  },
+  {
+    "revision": "f2249ac1c2fe72f04649ffa85217bbe6",
+    "url": "/ebcd/static/media/legend.f2249ac1.svg"
+  },
+  {
+    "revision": "08b347bee9e1b809187dc2dddf15b5d1",
+    "url": "/ebcd/static/media/location.08b347be.svg"
+  },
+  {
+    "revision": "3f99a64d3d2249a36f8c2dcffdcc18a2",
+    "url": "/ebcd/static/media/map.3f99a64d.svg"
+  },
+  {
+    "revision": "028883037118c193601fc06fa4ddeb9e",
+    "url": "/ebcd/static/media/pass.02888303.svg"
+  },
+  {
+    "revision": "f0d974cacf9ab5d136c47a289b5efc38",
+    "url": "/ebcd/static/media/summit.f0d974ca.svg"
+  },
+  {
+    "revision": "3099eae716147b6fd5959a0585686a86",
+    "url": "/ebcd/static/media/tent.3099eae7.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ebc-dashboard/precache-manifest.7a93efb6591c024bc08377666cab4385.js"
+  "/ebcd/precache-manifest.005978261b3761fd4b5a172599d96c7a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ebc-dashboard/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ebcd/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
